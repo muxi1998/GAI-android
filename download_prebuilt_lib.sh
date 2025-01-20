81778ed11f199ead9621ca261eeb467d347e4804
muxi <louis19980927@gmail.com>
2025-01-20 17:22:53 +0800

    [Open Source] update executorch prebuild library download script

--- download_prebuilt_lib.sh
+++ download_prebuilt_lib.sh
@@ -7,14 +7,13 @@
 
 set -eu
 
-AAR_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/0.4/executorch.aar"
-AAR_SHASUM_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/0.4/executorch.aar.sha256sums"
+AAR_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/executorch-241002/executorch.aar"
+AAR_SHASUM_URL="https://ossci-android.s3.us-west-1.amazonaws.com/executorch/release/executorch-241002/executorch.aar.sha256sums"
 LIBS_PATH="$(dirname "$0")/app/libs"
 
 mkdir -p "$LIBS_PATH"
 
 pushd "$LIBS_PATH"
 curl -O "${AAR_SHASUM_URL}"
-sed -i -e 's/executorch.aar/executorch-llama.aar/g' executorch.aar.sha256sums
-shasum --check --status executorch.aar.sha256sums || curl "${AAR_URL}" -o executorch-llama.aar
+shasum --check --status executorch.aar.sha256sums || curl "${AAR_URL}" -o executorch.aar
 popd
